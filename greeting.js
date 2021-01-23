cea70401dc8a4331affd968686ca994d7926cdb4
yslee7087 <76897109+yslee7087@users.noreply.github.com>
2021-01-23 16:12:52 +0900

    2nd version

--- greeting.js
+++ greeting.js
@@ -1,26 +1,38 @@
-const form = document.querySelector('.js-form');
-const input = form.querySelector('input');
-const gretting = document.querySelector('.js-greetings');
+const form = document.querySelector('.js-form'),
+  input = form.querySelector('input'),
+  greeting = document.querySelector('.js-greeting');
 
-const userLS = "currentUser";
-const showingCN = 'showing';
+const USER_LS = 'correntUser',
+  SHOWING_CN = 'showing';
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+}
+
+function askName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener('submit', handleSubmit)
+}
 
 function paintGreeting(text) {
-  form.classList.remove(showingCN);
-  greeting.classList.add(showingCN);
-  greeting.innerText = `Hello ${text}`
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
 }
 
 function loadName() {
-  const currentUser = localStorage.getItem(userLS);
-  if (currentUser === null){
-
+  const currentUser = localStorage.getItem(USER_LS);
+  if(currentUser === null) {
+    askName();
   } else {
-    paintGreeting(currentUser);
+    paintGreeting();
   }
 }
 
 function init() {
-
+  loadName();
 }
+
 init();
