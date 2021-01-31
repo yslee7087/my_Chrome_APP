0deb5171e055fb1316a65be62deaa1a6d7decc5e
yslee7087 <76897109+yslee7087@users.noreply.github.com>
2021-01-31 16:43:17 +0900

    4th version(not working well)

--- greeting.js
+++ greeting.js
@@ -2,38 +2,38 @@ const form = document.querySelector(".js-greetingForm"),
   input = form.querySelector("input"),
   greeting = document.querySelector(".js-greetings");
 const USER_LS = "currentUser",
-  SHOWING_CN = "showing";
+SHOWING_CN = "showing";
 
 function saveName(text) {
-  localStorage.setItem(USER_LS, text);
+localStorage.setItem(USER_LS, text);
 }
 
 function handleSubmit(event) {
-  event.preventDefault();
-  const currentValue = input.value;
-  paintGreeting(currentValue);
-  saveName(currentValue);
+event.preventDefault();
+const currentValue = input.value;
+paintGreeting(currentValue);
+saveName(currentValue);
 }
 
 function askForName() {
-  form.classList.add(SHOWING_CN);
-  form.addEventListener("submit", handleSubmit);
+form.classList.add(SHOWING_CN);
+form.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text) {
-  form.classList.remove(SHOWING_CN);
-  greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text}`;
+form.classList.remove(SHOWING_CN);
+greeting.classList.add(SHOWING_CN);
+greeting.innerText = `안녕하세요, ${text} 님!`;
 }
 function loadName() {
-  const currentUser = localStorage.getItem(USER_LS);
-  if (currentUser === null) {
-    askForName();
-  } else {
-    paintGreeting(currentUser);
-  }
+const currentUser = localStorage.getItem(USER_LS);
+if (currentUser === null) {
+  askForName();
+} else {
+  paintGreeting(currentUser);
+}
 }
 function init() {
-  loadName();
+loadName();
 }
 init();
